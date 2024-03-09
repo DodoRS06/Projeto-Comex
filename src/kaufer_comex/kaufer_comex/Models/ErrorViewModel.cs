6f4fc1514705614a795d5c960404ec8ed0e213bd
Karen Noguti <jkjvida@hotmail.com>
2024-03-09 23:06:30 +0900

    Projeto Base

---
+++ src/kaufer_comex/kaufer_comex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kaufer_comex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
