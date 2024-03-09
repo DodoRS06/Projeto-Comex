c46aa90539f1776870038ed3c57d2892038f804f
Claudia Carapia <ccarapiaf@gmail.com>
2024-03-09 18:50:26 -0300

    Update AppDbContext.cs

--- src/kaufer_comex/kaufer_comex/Models/AppDbContext.cs
+++ src/kaufer_comex/kaufer_comex/Models/AppDbContext.cs
@@ -5,5 +5,7 @@ namespace kaufer_comex.Models
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Processo> Processos { get; set; }
     }
 }
