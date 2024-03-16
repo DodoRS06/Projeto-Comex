3cd498f3c2dc5f3414deccadae9b2610d29b5231
keirosu <103314640+keirosu@users.noreply.github.com>
2024-03-17 07:49:16 +0900

    Criação do Model Despachante.

---
+++ src/kaufer_comex/kaufer_comex/Models/Despachante.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("Despachante")]
+    public class Despachante
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Despachante (*)")]
+        [Required(ErrorMessage = "Obrigatório informar o Despachante !")]
+        public string NomeDespachante { get; set; }
+    }
+}
+
