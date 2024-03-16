3d2ba5c8b46e26d994f06ea3a67e8db20235c850
Karen Noguti <jkjvida@hotmail.com>
2024-03-17 04:22:49 +0900

    Criação do Models Agente de Carga

---
+++ src/kaufer_comex/kaufer_comex/Models/AgenteDeCarga.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("AgenteDeCargas")]
+    public class AgenteDeCarga
+
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Agente de Carga (*)")]
+        [Required(ErrorMessage ="Obrigatório selecionar o Agente de Carga!")]
+        public string NomeAgenteCarga { get; set; }
+    }
+}
