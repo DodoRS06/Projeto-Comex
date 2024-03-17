3a0086775847a152e036153f747c3f47dd947d1c
Karen Noguti <jkjvida@hotmail.com>
2024-03-17 20:54:24 +0900

    Criação do Editar Agente de Cargas

--- src/kaufer_comex/kaufer_comex/Controllers/AgenteDeCargasController.cs
+++ src/kaufer_comex/kaufer_comex/Controllers/AgenteDeCargasController.cs
@@ -35,5 +35,32 @@ namespace kaufer_comex.Controllers
             }
             return View(agenteDeCarga);
         }
+
+        public async Task<IActionResult> Edit (int? id)
+        {
+            if (id == null)
+                return  NotFound();
+
+            var dados = await _context.AgenteDeCargas.FindAsync(id);
+            if(dados == null)
+                    return NotFound();
+
+            return View(dados);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, AgenteDeCarga agenteDeCarga)
+        {
+            if (id != agenteDeCarga.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.AgenteDeCargas.Update(agenteDeCarga);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
