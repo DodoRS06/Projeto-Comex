c4777eaacee9c125f90f3f90e01549240c9b5464
Karen Noguti <jkjvida@hotmail.com>
2024-03-17 21:21:00 +0900

    Criacão do Deletar Agente de Carga

--- src/kaufer_comex/kaufer_comex/Controllers/AgenteDeCargasController.cs
+++ src/kaufer_comex/kaufer_comex/Controllers/AgenteDeCargasController.cs
@@ -75,5 +75,36 @@ namespace kaufer_comex.Controllers
 
             return View(dados);
         }
+
+      
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.AgenteDeCargas.FindAsync(id);
+
+            if (id == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.AgenteDeCargas.FindAsync(id);
+
+            if (id == null)
+                return NotFound();
+
+            _context.AgenteDeCargas.Remove(dados);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
     }
 }
