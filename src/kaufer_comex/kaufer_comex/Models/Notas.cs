397bfa23e5f89d92dc9f65affe1862f17e10ee8d
Josyane Luiza Cupertino de Oliveira <103061826+josyaneluiza@users.noreply.github.com>
2024-03-19 21:16:39 -0300

    adicionando tabelas Item, Nota e Veiculo
    adicionando tabela Item , Nota Veiculo

---
+++ src/kaufer_comex/kaufer_comex/Models/Notas.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("Notas")]
+    public class Notas
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public string CodigoProduto { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public string DescricaoProduto { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public string Familia { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float Largura { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float Comprimento { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float Expressura { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float AreaM2 { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float Diametro { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float LarguraAparente { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float VolumeM2 { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float PesoLiquido { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public float PesoBruto { get; set; }
+    }
+}
---
+++ src/kaufer_comex/kaufer_comex/Models/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("Veiculo")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório")]
+        public string Motorista { get; set; }
+    }
+}
