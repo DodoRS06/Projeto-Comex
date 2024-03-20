1a79686bb33f6a82b5c47e95966bdf694e7939df
Josyane Luiza Cupertino de Oliveira <103061826+josyaneluiza@users.noreply.github.com>
2024-03-19 21:03:07 -0300

    adicionando tabela status
    adicionando tabela status

---
+++ src/kaufer_comex/kaufer_comex/Models/Status.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("Status")]
+    public class Status
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+    }
+}
