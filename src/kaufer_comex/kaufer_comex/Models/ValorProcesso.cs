9ecdc3f92daa06b2f13a088a0672ab11a8325ec2
keirosu <103314640+keirosu@users.noreply.github.com>
2024-03-24 10:21:08 +0900

    Criação valor

---
+++ src/kaufer_comex/kaufer_comex/Models/ValorProcesso.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("ValorProcessos")]
+    public class ValorProcesso
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Valor Exw (*)")]
+        [Required(ErrorMessage = "Obrigatório")]
+        public float ValorExw { get; set; }
+
+        [Display(Name = " Valor Fob/Fca ")]
+        public float ValorFobFca { get; set; }
+
+        [Display(Name = "Frete Internacional ")]
+        public float FreteInternacional { get; set; }
+
+        [Display(Name = "Seguro Internaciona")]
+        public float SeguroInternaciona { get; set; }
+
+        [Display(Name = "Valor Total (*)")]
+        [Required(ErrorMessage = "Obrigatório")]
+        public float ValorTotalCif { get; set; }
+
+        public Moeda Moeda { get; set; }
+
+
+    }
+
+    public enum Moeda
+    {
+       Real, 
+        Dólar,
+        Euro,
+    }
+}
