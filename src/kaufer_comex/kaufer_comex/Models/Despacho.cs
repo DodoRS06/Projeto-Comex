2a1c6a65e7d7f9f72fe481c5fff3af6bdfa963bc
Claudia Carapia <ccarapiaf@gmail.com>
2024-03-26 21:58:35 -0300

    Update Despacho.cs

--- src/kaufer_comex/kaufer_comex/Models/Despacho.cs
+++ src/kaufer_comex/kaufer_comex/Models/Despacho.cs
@@ -1,4 +1,4 @@
-﻿/*using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace kaufer_comex.Models
@@ -73,4 +73,4 @@ namespace kaufer_comex.Models
             Vermelho
         }
     }
-*/
+
