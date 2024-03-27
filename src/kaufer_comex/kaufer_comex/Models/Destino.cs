0a488b95ce0f553970deb9e29ac734b595467aea
Claudia Carapia <ccarapiaf@gmail.com>
2024-03-26 21:01:21 -0300

    Create Destino.cs

---
+++ src/kaufer_comex/kaufer_comex/Models/Destino.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace kaufer_comex.Models
+{
+    [Table("Destino")]
+    public class Destino
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string NomePais { get; set; }
+
+        public virtual ICollection<Processo> Processos { get; set; }
+    }
+}
