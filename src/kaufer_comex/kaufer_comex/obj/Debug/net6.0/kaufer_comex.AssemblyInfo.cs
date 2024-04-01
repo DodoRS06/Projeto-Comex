b736ec9eb266c36fb01128b4f7962c9b230581f3
Claudia Carapia <ccarapiaf@gmail.com>
2024-04-01 15:29:25 -0300

    Update Views

--- src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485f7ec6aa5c75cf25d73a4a524d5894decd55ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a614b442d1b2a6631c781e6cae8c9e64b84420be")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
