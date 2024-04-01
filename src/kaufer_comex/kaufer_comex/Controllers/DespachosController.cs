c8ae3e727f4fb51fe61415f967db87e519ed8488
Claudia Carapia <ccarapiaf@gmail.com>
2024-04-01 11:25:36 -0300

    Add DespachosController.cs

---
+++ src/kaufer_comex/kaufer_comex/Controllers/DespachosController.cs
@@ -0,0 +1,110 @@
+﻿using kaufer_comex.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace kaufer_comex.Controllers
+{
+    public class DespachosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DespachosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Despachos.ToListAsync();
+
+            return View(dados);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Despacho despacho)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Despachos.Add(despacho);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(despacho);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Despachos.FindAsync(id);
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Despacho despacho)
+        {
+            if (id != despacho.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Despachos.Update(despacho);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Despachos.FindAsync(id);
+
+            if (id == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Despachos.FindAsync(id);
+
+            if (id == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Despachos.FindAsync(id);
+
+            if (id == null)
+                return NotFound();
+
+            _context.Despachos.Remove(dados);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
