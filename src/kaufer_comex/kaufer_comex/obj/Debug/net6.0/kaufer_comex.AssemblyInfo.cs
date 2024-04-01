485f7ec6aa5c75cf25d73a4a524d5894decd55ff
Claudia Carapia <ccarapiaf@gmail.com>
2024-04-01 11:47:49 -0300

    Add Views Despachos

--- src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5a62fb9bc7039f128489a41faef56b305b12403")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
