a614b442d1b2a6631c781e6cae8c9e64b84420be
Claudia Carapia <ccarapiaf@gmail.com>
2024-04-01 12:21:58 -0300

    Update Migration M06Vendedor.cs

--- src/kaufer_comex/kaufer_comex/Migrations/20240401130918_M06Vendedor.cs
+++ src/kaufer_comex/kaufer_comex/Migrations/20240401130918_M06Vendedor.cs
@@ -9,19 +9,6 @@ namespace kaufer_comex.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Status",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    StatusAtual = table.Column<string>(type: "nvarchar(max)", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Status", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "Vendedores",
                 columns: table => new
                 {
@@ -37,9 +24,7 @@ namespace kaufer_comex.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "Status");
-
+           
             migrationBuilder.DropTable(
                 name: "Vendedores");
         }
