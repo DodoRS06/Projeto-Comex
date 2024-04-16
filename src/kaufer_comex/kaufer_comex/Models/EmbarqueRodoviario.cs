dc3bbe9495fd39108d631ee644fe7f88fb3e160d
Carolina-Laurindo <103545958+Carolina-Laurindo@users.noreply.github.com>
2024-04-15 22:03:20 -0300

    Revert "Merge branch 'main' of https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2024-1-e5-proj-empext-t5-comex"
    This reverts commit 2623492f15b23de250ddacda756941b180ee9627, reversing
    changes made to f61f28793bb1d57bc6c6e955e5dd0e2b7905ebb5.

--- src/kaufer_comex/kaufer_comex/Models/EmbarqueRodoviario.cs
+++ src/kaufer_comex/kaufer_comex/Models/EmbarqueRodoviario.cs
@@ -33,7 +33,7 @@ namespace kaufer_comex.Models
 
         [Display(Name = "Código do processo (*)")]
         [Required(ErrorMessage = "Obrigatório informar o código.")]
-        public int ProcessoId { get; set; }
+        public int IdProcesso { get; set; }
 
         [ForeignKey("ProcessoId")]
         public Processo Processo { get; set; }
