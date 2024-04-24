5402805cc78d4406ab53913036fbbbf210099848
Douglas Raynner <douglas.raynner@sga.pucminas.br>
2024-04-24 10:38:46 -0300

    Diminuição da quantidade de informação na index de ExpImp

--- src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8170a6da5f471421ce54bbc71b6bb98017fc2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
