5da72275866ef90f1ef23b2581833f787be6a552
keirosu <103314640+keirosu@users.noreply.github.com>
2024-05-11 18:43:50 +0900

    Update valor processo.

--- src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8737acdf7920bb22c2c2219fe4cb61b9d66deea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
