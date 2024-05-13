d1ee86879c8d0ad433f70df70b883843bbbbcc00
Claudia Carapia <ccarapiaf@gmail.com>
2024-05-13 20:53:39 -0300

    Atualização Migração Itens

--- src/kaufer_comex/kaufer_comex/Migrations/20240512002431_UpdateItens.cs
+++ src/kaufer_comex/kaufer_comex/Migrations/20240512002431_UpdateItens.cs
@@ -13,34 +13,18 @@ namespace kaufer_comex.Migrations
                 table: "Itens",
                 newName: "Espessura");
 
-            migrationBuilder.RenameColumn(
-                name: "Diametro",
-                table: "Itens",
-                newName: "DiametroComprimento");
-
-            migrationBuilder.AddColumn<float>(
-                name: "DiametroAltura",
-                table: "Itens",
-                type: "real",
-                nullable: false,
-                defaultValue: 0f);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "DiametroAltura",
-                table: "Itens");
+           
 
             migrationBuilder.RenameColumn(
                 name: "Espessura",
                 table: "Itens",
                 newName: "Expressura");
 
-            migrationBuilder.RenameColumn(
-                name: "DiametroComprimento",
-                table: "Itens",
-                newName: "Diametro");
+ 
         }
     }
 }
--- src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Debug/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1df3f1d3a66b3e6fef78d41449682b3c5b45fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
