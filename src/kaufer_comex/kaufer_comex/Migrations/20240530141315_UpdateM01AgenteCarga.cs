b5c511ef1edd744000c1b9e833a2250d7c3fdcd8
Karen Noguti <jkjvida@hotmail.com>
2024-05-31 01:15:11 +0900

    Update Agente de Carga

---
+++ src/kaufer_comex/kaufer_comex/Migrations/20240530141315_UpdateM01AgenteCarga.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kaufer_comex.Migrations
+{
+    public partial class UpdateM01AgenteCarga : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Proforma",
+                table: "Processo",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CertificadoQualidade",
+                table: "Notas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConhecimentoEmbarque",
+                table: "Despacho",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contato",
+                table: "AgenteDeCargas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "AgenteDeCargas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "AgenteDeCargas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "DCETemp",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CadastroDespesaId = table.Column<int>(type: "int", nullable: false),
+                    CadastroDespesaNome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FornecedorServicoId = table.Column<int>(type: "int", nullable: false),
+                    FornecedorServicoNome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Valor = table.Column<float>(type: "real", nullable: false),
+                    Observacao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProcessoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DCETemp", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DCETemp");
+
+            migrationBuilder.DropColumn(
+                name: "Contato",
+                table: "AgenteDeCargas");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "AgenteDeCargas");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "AgenteDeCargas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Proforma",
+                table: "Processo",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CertificadoQualidade",
+                table: "Notas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConhecimentoEmbarque",
+                table: "Despacho",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
