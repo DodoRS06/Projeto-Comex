83a7db0ab535d1be48319be483677558f012823a
Josyane Luiza Cupertino de Oliveira <103061826+josyaneluiza@users.noreply.github.com>
2024-06-01 17:20:00 -0300

    Alterações de Estilo em Itens
    Alterações em details

--- src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24688999783f08575f1271f8bca3a56a40c29060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+015c085f38f1964c206cc4b9f9bf087ea641ab40")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
