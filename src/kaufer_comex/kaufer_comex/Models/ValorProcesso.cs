f5dfa941ffa2d92d728c6ab7ef08fd221b368591
Karen Noguti <jkjvida@hotmail.com>
2024-06-01 21:12:43 +0900

    ValorProcesso UPDATE Create, Edit, Details, Program.

--- src/kaufer_comex/kaufer_comex/Models/ValorProcesso.cs
+++ src/kaufer_comex/kaufer_comex/Models/ValorProcesso.cs
@@ -12,28 +12,28 @@ namespace kaufer_comex.Models
         [Display(Name = "Valor Exw (*)")]
         [Column(TypeName = "decimal(18,2)")]
         [Required(ErrorMessage = "Obrigatório")]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal ValorExw { get; set; }
 
         [Display(Name = " Valor Fob/Fca ")]
         [Column(TypeName = "decimal(18,2)")]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal ValorFobFca { get; set; }
 
         [Display(Name = "Frete Internacional ")]
         [Column(TypeName = "decimal(18,2)")]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal FreteInternacional { get; set; }
 
         [Display(Name = "Seguro Internacional")]
         [Column(TypeName = "decimal(18,2)")]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal SeguroInternaciona { get; set; }
 
         [Display(Name = "Valor Total (*)")]
         [Column(TypeName = "decimal(18,2)")]
         [Required(ErrorMessage = "Obrigatório")]
-        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public decimal ValorTotalCif { get; set; }
 
 		
