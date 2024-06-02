126bcc63f955c5023dd1ae4007289b54944a4d4f
Karen Noguti <jkjvida@hotmail.com>
2024-06-03 08:17:21 +0900

    Update Layout

--- src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0035c17712cedd47ca277a40f8ec00690fa113c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
