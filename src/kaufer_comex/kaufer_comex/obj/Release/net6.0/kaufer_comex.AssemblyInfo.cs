5ae86cb126885cbfca4038e4ed9bcc528b7da4f0
Claudia Carapia <ccarapiaf@gmail.com>
2024-06-05 14:49:53 -0300

    Att

--- src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
+++ src/kaufer_comex/kaufer_comex/obj/Release/net6.0/kaufer_comex.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8469d29f54093f48d0053eea9f1014d5920b7159")]
 [assembly: System.Reflection.AssemblyProductAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kaufer_comex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
